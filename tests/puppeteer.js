1371ef40b3a54a5043bd5973f190e8e6f97339eb
Bharath Kumar <bharath.a@minfytech.com>
2020-01-31 15:15:18 +0530

    Fixed Puppeteer Error

--- tests/puppeteer.js
+++ tests/puppeteer.js
@@ -1,7 +1,13 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch(
+    {
+        headless: true,
+        executablePath:'/var/lib/jenkins/workspace/Puppeteer-Tests@2/node_modules/puppeteer/.local-chromium/linux-722234/chrome-linux/chrome',
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    }
+  );
   const page = await browser.newPage();
   await page.goto('https://example.com', {waitUntil: 'networkidle2'});
   await page.pdf({path: 'hn.pdf', format: 'A4'});
